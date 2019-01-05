68f2e6ee6971c16577de44eb496a8020b35b40e2
tzuhsienliu <tzuhsienliu@email.arizona.edu>
2019-01-05 14:17:31 -0700

    moving and general board setup

--- js/events.js
+++ js/events.js
@@ -1,4 +1,50 @@
-let element = document.querySelector(".title");
-element.onclick = function(){
-	console.log("hello");
-};
+
+// Steven Chin & Tzuhsien Liu
+
+// On document loaded init Game
+document.addEventListener("DOMContentLoaded", e => {
+  let selected = false;
+  let prevPiece;
+  let game = new Game();
+  console.log("Game Started");
+
+  //Select a piece
+  document.addEventListener("click", e =>{
+    let target = e.target;
+    let isPiece = target.classList.contains("piece");
+
+    if (isPiece && !selected) {
+      target.classList.toggle("selected");
+      prevPiece = target;
+      selected = true;
+      // console.log("Piece selected");
+    } else if (isPiece && selected) {
+      target.classList.toggle("selected");
+      if(prevPiece !== target){
+        prevPiece.classList.remove("selected");
+      }
+      prevPiece = target;
+      selected = true;
+      // console.log("Piece selected2");
+    }
+  });
+
+  //Move the piece
+  document.addEventListener("click", e =>{
+    let target = e.target;
+    let isBlock = target.classList.contains("black");
+    if (isBlock && selected) {
+      if(prevPiece){
+        prevPiece.classList.remove("selected");
+      }
+      prevPiece.parentNode.removeChild(prevPiece);
+      target.appendChild(prevPiece);
+      selected = false;
+      console.log("Made a move");
+    }
+  });
+
+});
+
+
+
